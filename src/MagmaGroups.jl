2ea8e89cc179cbfdcf4e225d1cbfbf496f5709ff
Tommy Hofmann <thofma@gmail.com>
2023-07-21 22:56:33 +0200

    first start

--- src/MagmaGroups.jl
+++ src/MagmaGroups.jl
@@ -2,4 +2,24 @@ module MagmaGroups
 
 # Write your package code here.
 
+using Oscar
+using MagmaCall
+
+import Oscar: IntegerUnion, automorphism_group, abelian_group, isomorphism, ngens, permutation_group, hom, domain, codomain, gen, image, preimage
+
+_prepare_for_magma(x::ZZRingElem) = BigInt(x)
+_prepare_for_magma(x::Vector) = _prepare_for_magma.(x)
+_prepare_for_magma(x) = x
+
+abstract type Magma end
+
+abstract type MagmaGroup end
+
+include("Generic.jl")
+include("PermGroup.jl")
+include("AbelianGroup.jl")
+
+
+export Magma
+
 end
